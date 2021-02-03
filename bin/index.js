0968af812f399bfe11e2c345a179838666dc43c5
Troy Moreland <49797484+34fame@users.noreply.github.com>
2021-02-03 16:45:29 -0600

    added query-firestore and introduced parameters

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 const manageNodeModules = require('../lib/manage-node-modules')
 const searchReplace = require('../lib/search-replace')
-const resetFirebase = require('../lib/reset-firestore')
+const queryFirestore = require('../lib/query-firestore')
+const resetFirestore = require('../lib/reset-firestore')
 
 const program = require('commander')
 
@@ -22,11 +23,29 @@ program
    })
 
 program
+   .command('query-firestore [project] [collection] [where]')
+   .alias('qfs')
+   .description('Get Doc IDs from a firestore collection')
+   .action(function (project, collection, where) {
+      if (where) {
+         let isValid = false
+         try {
+            isValid = Array.isArray(eval(where))
+         } catch (err) {
+            console.log("The WHERE parameter must be an array encapsulated in a string: \"['foo', '==', 'bar']\"")
+            return false
+         }
+      }
+      const payload = { project, collection, where: where ? eval(where) : null }
+      queryFirestore(payload)
+   })
+
+program
    .command('reset-firestore')
    .alias('reset-fs')
    .description('Wipes data & re-populates with bootstrap data')
    .action(function () {
-      resetFirebase()
+      resetFirestore()
    })
 
 program.parse(process.argv)
